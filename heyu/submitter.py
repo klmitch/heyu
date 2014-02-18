0612c2b0bd33458630d4438bd4995925d04c64e6
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2014-02-17 18:18:52 -0600

    Create the submitter application
    This is the basic submitter application (sans CERTs, for now).  The
    submitter sends a notification to a hub.

---
+++ heyu/submitter.py
@@ -0,0 +1,193 @@
+# Copyright 2014 Rackspace
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from __future__ import print_function
+
+import os
+import sys
+
+import cli_tools
+import tendril
+
+from heyu import protocol
+from heyu import util
+
+
+class SubmitterException(Exception):
+    """
+    An exception for reporting errors with the submitter.
+    """
+
+    pass
+
+
+class SubmitterApplication(tendril.Application):
+    def __init__(self, parent, app_name, summary, body,
+                 urgency=None, category=None, id=None):
+        """
+        Initialize a submitter application.  This submits the notification
+        to the hub.
+
+        :param parent: The parent of the ``SubmitterApplication``.
+                       This will be an instance of
+                       ``tendril.Tendril``.
+        :param app_name: The name of the application the notification
+                         is for.
+        :param summary: A summary of the notification.
+        :param body: The body of the notification.
+        :param urgency: The urgency level for the notification.
+                        Optional.
+        :param category: A category for the notification.  Optional.
+        :param id: The ID of a notification to replace.  Optional.
+        """
+
+        # Initialize the application
+        super(SubmitterApplication, self).__init__(parent)
+
+        # Set up the desired framer
+        parent.framers = tendril.COBSFramer(True)
+
+        # Create the notify message
+        kwargs = {
+            'app_name': app_name,
+            'summary': summary,
+            'body': body,
+        }
+        if urgency is not None:
+            kwargs['urgency'] = urgency
+        if category is not None:
+            kwargs['category'] = category
+        if id is not None:
+            kwargs['id'] = id
+        msg = protocol.Message('notify', **kwargs)
+
+        # Send it
+        self.send_frame(msg.to_frame())
+
+    def recv_frame(self, frame):
+        """
+        Called when a frame is received.  Prints out the notification ID.
+
+        :param frame: The received frame.
+        """
+
+        # Parse the frame
+        try:
+            msg = protocol.Message.from_frame(frame)
+            if msg.msg_type == 'accepted':
+                print(msg.id)
+            elif msg.msg_type == 'error':
+                print('Failed to submit notification: %s' % msg.reason,
+                      file=sys.stderr)
+            else:
+                print('Unrecognized protocol message "%s"' % msg.msg_type,
+                      file=sys.stderr)
+        except ValueError as e:
+            print('Failed to parse frame: %s' % e, file=sys.stderr)
+
+        # Close the connection
+        self.close()
+
+
+@cli_tools.argument('summary',
+                    help='Summary of the notification.')
+@cli_tools.argument('body',
+                    nargs='?',
+                    default='',
+                    help='Body of the notification.')
+@cli_tools.argument('--urgency', '-u',
+                    default=None,
+                    help='Specifies the urgency level '
+                    '(low, normal, critical).')
+@cli_tools.argument('--app-name', '-a',
+                    default=None,
+                    help='Specifies the application name.')
+@cli_tools.argument('--category', '-c',
+                    default=None,
+                    help='Specifies the notification category.')
+@cli_tools.argument('--host', '-H',
+                    default=None,
+                    help='Specifies the HeyU hub to submit the '
+                    'notification to, as "hostname" or "hostname:port".')
+@cli_tools.argument('--id', '-I',
+                    default=None,
+                    help='Specifies the ID of a notification to replace.')
+def send_notification(hub, app_name, summary, body,
+                      urgency=None, category=None, id=None):
+    """
+    Sends a notification via the configured HeyU hub.  The hub address
+    is read from the "~/.heyu.hub" file, which should contain either
+    "hostname" or "hostname:port".  If the file doesn't exist, and
+    "--host" is not given, "localhost" will be tried.  Prints out the
+    notification ID if the notification is accepted.
+
+    :param hub: The address of the hub, as a tuple of hostname and
+                port.
+    :param app_name: The name of the application the notification is
+                     for.
+    :param summary: A summary of the notification.
+    :param body: The body of the notification.
+    :param urgency: The urgency level for the notification.  Optional.
+    :param category: A category for the notification.  Optional.
+    :param id: The ID of a notification to replace.  Optional.
+    """
+
+    # Look up the manager
+    manager = tendril.get_manager('tcp')
+    manager.start()
+
+    # Connect to the hub
+    app = tendril.TendrilPartial(SubmitterApplication,
+                                 app_name, summary, body,
+                                 urgency, category, id)
+    manager.connect(hub, app)
+
+
+@send_notification.processor
+def _normalize_args(args):
+    """
+    Pre-process arguments before calling ``send_notification()``.
+    This ensures the arguments are normalized.
+
+    :param args: The values of the command line arguments for
+                 normalization.
+    """
+
+    # Start off with the hub data
+    hub = args.hub
+    if hub is None:
+        try:
+            with open(os.path.expanduser('~/.heyu.hub')) as f:
+                hub = f.read().strip()  # pragma: no cover
+        except IOError:
+            hub = None
+
+    # Do we have a hub?
+    if not hub:
+        args.hub = ('127.0.0.1', util.HEYU_PORT)
+    else:
+        args.hub = util.parse_hub(hub)
+
+    # Next, we need the application name
+    if not args.app_name:
+        args.app_name = os.path.basename(sys.argv[0])
+
+    # Now, decode the urgency
+    if args.urgency:
+        urgency = protocol.urgency_map.get(args.urgency.lower())
+        if urgency is None:
+            raise SubmitterException("Unknown urgency level '%s'" %
+                                     args.urgency)
+        args.urgency = urgency
---
+++ tests/unit/test_submitter.py
@@ -0,0 +1,255 @@
+# Copyright 2014 Rackspace
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from __future__ import print_function
+
+import io
+import sys
+import unittest
+
+import mock
+
+from heyu import protocol
+from heyu import submitter
+from heyu import util
+
+
+class SubmitterApplicationTest(unittest.TestCase):
+    @mock.patch('tendril.COBSFramer', return_value='framer')
+    @mock.patch.object(protocol, 'Message', return_value=mock.Mock(**{
+        'to_frame.return_value': 'message',
+    }))
+    @mock.patch.object(submitter.SubmitterApplication, 'send_frame')
+    def test_init_basic(self, mock_send_frame, mock_Message, mock_COBSFramer):
+        parent = mock.Mock()
+
+        app = submitter.SubmitterApplication(parent, 'app', 'summary', 'body')
+
+        self.assertEqual(parent, app.parent)
+        mock_COBSFramer.assert_called_once_with(True)
+        self.assertEqual('framer', parent.framers)
+        mock_Message.assert_called_once_with(
+            'notify', app_name='app', summary='summary', body='body')
+        mock_send_frame.assert_called_once_with('message')
+
+    @mock.patch('tendril.COBSFramer', return_value='framer')
+    @mock.patch.object(protocol, 'Message', return_value=mock.Mock(**{
+        'to_frame.return_value': 'message',
+    }))
+    @mock.patch.object(submitter.SubmitterApplication, 'send_frame')
+    def test_init_extra(self, mock_send_frame, mock_Message, mock_COBSFramer):
+        parent = mock.Mock()
+
+        app = submitter.SubmitterApplication(parent, 'app', 'summary', 'body',
+                                             'urgency', 'category', 'id')
+
+        self.assertEqual(parent, app.parent)
+        mock_COBSFramer.assert_called_once_with(True)
+        self.assertEqual('framer', parent.framers)
+        mock_Message.assert_called_once_with(
+            'notify', app_name='app', summary='summary', body='body',
+            urgency='urgency', category='category', id='id')
+        mock_send_frame.assert_called_once_with('message')
+
+    @mock.patch.object(submitter.SubmitterApplication, '__init__',
+                       return_value=None)
+    @mock.patch.object(submitter.SubmitterApplication, 'close')
+    @mock.patch.object(protocol.Message, 'from_frame', return_value=mock.Mock(
+        msg_type='accepted', id='notification-id'))
+    @mock.patch('__builtin__.print')
+    def test_recv_frame_accepted(self, mock_print, mock_from_frame,
+                                 mock_close, mock_init):
+        app = submitter.SubmitterApplication()
+
+        app.recv_frame('frame')
+
+        mock_from_frame.assert_called_once_with('frame')
+        mock_print.assert_called_once_with('notification-id')
+        mock_close.assert_called_once_with()
+
+    @mock.patch.object(submitter.SubmitterApplication, '__init__',
+                       return_value=None)
+    @mock.patch.object(submitter.SubmitterApplication, 'close')
+    @mock.patch.object(protocol.Message, 'from_frame', return_value=mock.Mock(
+        msg_type='error', reason='something bad happened'))
+    @mock.patch('__builtin__.print')
+    def test_recv_frame_error(self, mock_print, mock_from_frame,
+                              mock_close, mock_init):
+        app = submitter.SubmitterApplication()
+
+        app.recv_frame('frame')
+
+        mock_from_frame.assert_called_once_with('frame')
+        mock_print.assert_called_once_with(
+            'Failed to submit notification: something bad happened',
+            file=sys.stderr)
+        mock_close.assert_called_once_with()
+
+    @mock.patch.object(submitter.SubmitterApplication, '__init__',
+                       return_value=None)
+    @mock.patch.object(submitter.SubmitterApplication, 'close')
+    @mock.patch.object(protocol.Message, 'from_frame', return_value=mock.Mock(
+        msg_type='other'))
+    @mock.patch('__builtin__.print')
+    def test_recv_frame_unknown(self, mock_print, mock_from_frame,
+                                mock_close, mock_init):
+        app = submitter.SubmitterApplication()
+
+        app.recv_frame('frame')
+
+        mock_from_frame.assert_called_once_with('frame')
+        mock_print.assert_called_once_with(
+            'Unrecognized protocol message "other"',
+            file=sys.stderr)
+        mock_close.assert_called_once_with()
+
+    @mock.patch.object(submitter.SubmitterApplication, '__init__',
+                       return_value=None)
+    @mock.patch.object(submitter.SubmitterApplication, 'close')
+    @mock.patch.object(protocol.Message, 'from_frame',
+                       side_effect=ValueError('bad frame'))
+    @mock.patch('__builtin__.print')
+    def test_recv_frame_parse_error(self, mock_print, mock_from_frame,
+                                    mock_close, mock_init):
+        app = submitter.SubmitterApplication()
+
+        app.recv_frame('frame')
+
+        mock_from_frame.assert_called_once_with('frame')
+        mock_print.assert_called_once_with(
+            'Failed to parse frame: bad frame',
+            file=sys.stderr)
+        mock_close.assert_called_once_with()
+
+
+class SendNotificationTest(unittest.TestCase):
+    @mock.patch('tendril.get_manager')
+    @mock.patch('tendril.TendrilPartial', return_value='the_app')
+    def test_basic(self, mock_TendrilPartial, mock_get_manager):
+        submitter.send_notification('hub', 'app', 'summary', 'body')
+
+        mock_get_manager.assert_called_once_with('tcp')
+        mock_get_manager.return_value.assert_has_calls([
+            mock.call.start(),
+            mock.call.connect('hub', 'the_app'),
+        ])
+        mock_TendrilPartial.assert_called_once_with(
+            submitter.SubmitterApplication,
+            'app', 'summary', 'body', None, None, None)
+
+    @mock.patch('tendril.get_manager')
+    @mock.patch('tendril.TendrilPartial', return_value='the_app')
+    def test_extra(self, mock_TendrilPartial, mock_get_manager):
+        submitter.send_notification('hub', 'app', 'summary', 'body',
+                                    'urgency', 'category', 'id')
+
+        mock_get_manager.assert_called_once_with('tcp')
+        mock_get_manager.return_value.assert_has_calls([
+            mock.call.start(),
+            mock.call.connect('hub', 'the_app'),
+        ])
+        mock_TendrilPartial.assert_called_once_with(
+            submitter.SubmitterApplication,
+            'app', 'summary', 'body', 'urgency', 'category', 'id')
+
+
+class NormalizeArgsTest(unittest.TestCase):
+    @mock.patch('os.path.expanduser', return_value='/home/dir/.heyu.hub')
+    @mock.patch('__builtin__.open', side_effect=IOError())
+    @mock.patch.object(util, 'parse_hub', return_value=('hub', 1234))
+    @mock.patch('sys.argv', ['my/submitter'])
+    def test_defaults(self, mock_parse_hub, mock_open, mock_expand_user):
+        args = mock.Mock(hub=None, app_name=None, urgency=None)
+
+        submitter._normalize_args(args)
+
+        self.assertEqual(('127.0.0.1', util.HEYU_PORT), args.hub)
+        self.assertEqual('submitter', args.app_name)
+        self.assertEqual(None, args.urgency)
+        mock_open.assert_called_once_with('/home/dir/.heyu.hub')
+        self.assertFalse(mock_parse_hub.called)
+
+    @mock.patch('os.path.expanduser', return_value='/home/dir/.heyu.hub')
+    @mock.patch('__builtin__.open', return_value=io.BytesIO('hub\n'))
+    @mock.patch.object(util, 'parse_hub', return_value=('hub', 1234))
+    @mock.patch('sys.argv', ['my/submitter'])
+    def test_read_hub(self, mock_parse_hub, mock_open, mock_expand_user):
+        args = mock.Mock(hub=None, app_name=None, urgency=None)
+
+        submitter._normalize_args(args)
+
+        self.assertEqual(('hub', 1234), args.hub)
+        self.assertEqual('submitter', args.app_name)
+        self.assertEqual(None, args.urgency)
+        mock_open.assert_called_once_with('/home/dir/.heyu.hub')
+        mock_parse_hub.assert_called_once_with('hub')
+
+    @mock.patch('os.path.expanduser', return_value='/home/dir/.heyu.hub')
+    @mock.patch('__builtin__.open', side_effect=IOError())
+    @mock.patch.object(util, 'parse_hub', return_value=('hub', 1234))
+    @mock.patch('sys.argv', ['my/submitter'])
+    def test_given_hub(self, mock_parse_hub, mock_open, mock_expand_user):
+        args = mock.Mock(hub='hub', app_name=None, urgency=None)
+
+        submitter._normalize_args(args)
+
+        self.assertEqual(('hub', 1234), args.hub)
+        self.assertEqual('submitter', args.app_name)
+        self.assertEqual(None, args.urgency)
+        self.assertFalse(mock_open.called)
+        mock_parse_hub.assert_called_once_with('hub')
+
+    @mock.patch('os.path.expanduser', return_value='/home/dir/.heyu.hub')
+    @mock.patch('__builtin__.open', side_effect=IOError())
+    @mock.patch.object(util, 'parse_hub', return_value=('hub', 1234))
+    @mock.patch('sys.argv', ['my/submitter'])
+    def test_given_app_name(self, mock_parse_hub, mock_open, mock_expand_user):
+        args = mock.Mock(hub=None, app_name='myapp', urgency=None)
+
+        submitter._normalize_args(args)
+
+        self.assertEqual(('127.0.0.1', util.HEYU_PORT), args.hub)
+        self.assertEqual('myapp', args.app_name)
+        self.assertEqual(None, args.urgency)
+        mock_open.assert_called_once_with('/home/dir/.heyu.hub')
+        self.assertFalse(mock_parse_hub.called)
+
+    @mock.patch('os.path.expanduser', return_value='/home/dir/.heyu.hub')
+    @mock.patch('__builtin__.open', side_effect=IOError())
+    @mock.patch.object(util, 'parse_hub', return_value=('hub', 1234))
+    @mock.patch('sys.argv', ['my/submitter'])
+    def test_given_urgency(self, mock_parse_hub, mock_open, mock_expand_user):
+        args = mock.Mock(hub=None, app_name=None, urgency='LoW')
+
+        submitter._normalize_args(args)
+
+        self.assertEqual(('127.0.0.1', util.HEYU_PORT), args.hub)
+        self.assertEqual('submitter', args.app_name)
+        self.assertEqual(protocol.URGENCY_LOW, args.urgency)
+        mock_open.assert_called_once_with('/home/dir/.heyu.hub')
+        self.assertFalse(mock_parse_hub.called)
+
+    @mock.patch('os.path.expanduser', return_value='/home/dir/.heyu.hub')
+    @mock.patch('__builtin__.open', side_effect=IOError())
+    @mock.patch.object(util, 'parse_hub', return_value=('hub', 1234))
+    @mock.patch('sys.argv', ['my/submitter'])
+    def test_bad_urgency(self, mock_parse_hub, mock_open, mock_expand_user):
+        args = mock.Mock(hub=None, app_name=None, urgency='High')
+
+        self.assertRaises(submitter.SubmitterException,
+                          submitter._normalize_args, args)
+
+        mock_open.assert_called_once_with('/home/dir/.heyu.hub')
+        self.assertFalse(mock_parse_hub.called)
